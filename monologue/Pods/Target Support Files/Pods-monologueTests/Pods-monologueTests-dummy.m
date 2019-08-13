bd59dfacba5a59fa1348d8bce7ea8f85e32bfc5b
zyuan4 <zyuan4@student.unimelb.edu.au>
2019-08-13 19:13:50 +1000

    Initialise the file and link Firebase database to it

---
+++ monologue/Pods/Target Support Files/Pods-monologueTests/Pods-monologueTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_monologueTests : NSObject
+@end
+@implementation PodsDummy_Pods_monologueTests
+@end
---
+++ monologue/monologue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  monologue
+//
+//  Created by 袁翥 on 2019/8/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
