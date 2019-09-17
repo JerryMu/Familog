b7619b170cd6b22aea8a3c63a4b71738d8814ea2
Ziyuan Xiao <ziyuanx@student.unimelb.edu.au>
2019-09-17 17:56:01 +1000

    test

--- monologue/monologue/MainPage/ProfileTableViewController.swift
+++ monologue/monologue/MainPage/ProfileTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfileController: UITableViewController {
+class ProfileTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
