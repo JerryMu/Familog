a335d3787baab0a2f22d2878caea58ca6714bbbe
jim1997 <shishengl@student.unimelb.edu.au>
2019-09-17 18:18:14 +1000

    test

--- monologue/monologue/MainPage/ProfileTableViewController.swift
+++ monologue/monologue/MainPage/ProfileTableViewController.swift
@@ -11,7 +11,7 @@ class ProfileTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //test
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
         
