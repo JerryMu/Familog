37e0f1ed3add486a9bcf42111137cdcb753f91f3
Pengyu <Jerrymupengyu@gmail.com>
2019-09-21 18:55:42 +1000

    Auto Sign in fix

---
+++ monologue/monologue/LogInPage/LaunchScreen.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  Familog
+//
+//  Created by Pengyu Mu on 21/9/19.
+//
+
+import UIKit
+import FirebaseAuth
+
+class LaunchScreenViewController: UIViewController {
+   override func viewDidLoad() {
+       super.viewDidLoad()
+       // Do any additional setup after loading the view.
+   }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if Auth.auth().currentUser != nil {
+            Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: {(timer) in
+
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                let newViewController = storyboard.instantiateViewController(withIdentifier: "Tabbar") as! TabBarViewController
+                self.present(newViewController, animated: true, completion: nil)
+            })
+        }
+        else
+        {
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block: {(timer) in
+                
+            let storyboard = UIStoryboard(name: "Start", bundle: nil)
+            let newViewController = storyboard.instantiateViewController(withIdentifier: "Authentication") as! WelcomeViewController
+            self.present(newViewController, animated: true, completion: nil)
+            })
+        
+        }
+
+    }
+    
+}
--- monologue/monologue/LogInPage/WelcomeViewController.swift
+++ monologue/monologue/LogInPage/WelcomeViewController.swift
@@ -15,17 +15,5 @@
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        if Auth.auth().currentUser != nil {
-            Timer.scheduledTimer(withTimeInterval: 2, repeats: false, block: {(timer) in
-                
-                let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                let newViewController = storyboard.instantiateViewController(withIdentifier: "Tabbar") as! TabBarViewController
-                self.present(newViewController, animated: true, completion: nil)
-            })
-        }
-    }
-    
  }
  
