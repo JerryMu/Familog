63d1e3bb05acdc1ed79a4b48593d9742fc9ad18c
zyuan4 <zyuan4@student.unimelb.edu.au>
2019-10-04 15:11:18 +1000

    Rewrite Upload Function and FamilyApi

--- monologue/monologue/Api.swift
+++ monologue/monologue/Api.swift
@@ -11,4 +11,5 @@ import Foundation
 struct Api {
     static var User = UserApi()
     static var Post = PostApi()
+    static var Family = FamilyApi()
 }
--- monologue/monologue/FamilyApi.swift
+++ monologue/monologue/FamilyApi.swift
@@ -6,11 +6,26 @@
 //
 //
 import Foundation
-
+import FirebaseAuth
+import FirebaseFirestore
 
 class FamilyApi{
     
     func observeFamily(){
         
+    }
+    
+    let currentUser = Auth.auth().currentUser!.uid
+    let userRef = Firestore.firestore().collection("User")
+    var familyId:String = ""
+    
+    func getFamilyId() {
+        userRef.document(currentUser).getDocument {(document, error) in
+            if let document = document, document.exists {
+                self.familyId = document.get("familyId") as! String
+            } else {
+                print("Family does not exist")
+            }
+        }
     }
 }
