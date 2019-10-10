5ad576b59ddd331cd516d6e077e1d09fdcd92349
jim1997 <shishengl@student.unimelb.edu.au>
2019-10-10 15:48:42 +1100

    fix unit test bug

---
+++ monologue/monologueTest/UserTests.swift
@@ -0,0 +1,26 @@
+//
+//  UserTests.swift
+//  FamilogTests
+//
+//  Created by 刘仕晟 on 2019/10/10.
+//
+
+import Foundation
+import XCTest
+@testable import Familog
+
+class UserTests: XCTestCase {
+
+     func testUserfirstname() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+   
+
+
+
+
+
+
+
+}
