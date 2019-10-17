d4b658dc538ff17dfd074b15a7ae341ad2cbb2ab
Ziyuan Xiao <ziyuanx@student.unimelb.edu.au>
2019-10-17 22:00:23 +1100

    fix frontend bug

--- monologue/monologue/MainPage/ProfilePage/SwitchFamilyViewController.swift
+++ monologue/monologue/MainPage/ProfilePage/SwitchFamilyViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SwitchFamilyViewController: UIViewController {
+class SwitchFamilyViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
